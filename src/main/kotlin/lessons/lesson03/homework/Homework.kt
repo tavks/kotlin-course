422d1d9c6ff5d7f615ebeb1a6b70444f91ea2778
Vladimir Khramov <khramov.dev@gmail.com>
2025-09-14 22:23:09 +0200

    Lesson3 homework

---
+++ src/main/kotlin/lessons/lesson03/homework/Homework.kt
@@ -0,0 +1,51 @@
+package org.example.lessons.lesson03.homework
+
+val name: String = "Monster Hackaton"
+var date: String = "10 ноября 2025"
+val place: String = "Hackaton Place"
+private val budget: String = "подробная информация с перечислением поставщиков и сумм для внутреннего пользования"
+lateinit var participantsCount: Int
+val eventDuration: Int = 48
+private val legalInfo: String = "Контактная информация и условия соглашений с поставщиками пищи, оборудования и других услуг"
+var currentEventStatus: String = "Текущее состояние хакатона (статус)"
+val sponsors: String = "Список спонсоров"
+private val budgetLimit: Int = 1_000_000
+lateinit var currentInternetStatus: String
+private var internalCommunication: String = "Информация о транспортировке оборудования, распределении ресурсов и координации между различными командами поддержки"
+lateinit var teamQuantity: Int
+lateinit var tasks: String
+val evacuationPlan: String = "План эвакуации"
+private val availableEquipment = "Список доступного оборудования (всё, что выделено для использования на мероприятии)"
+var idleEquipment = "Список свободного оборудования (всё, что можно взять прямо сейчас и что не занято другими участниками)"
+lateinit var mealSchedule: String
+var disasterManagmentPlan: String = "План мероприятий на случай сбоев"
+var expertsAndJury: String = "Список экспертов и жюри"
+val privacyPolicy: String = "Политика конфиденциальности"
+lateinit private var feedback: String
+var currentTemperature: Int = 23
+var monitoringData: String by lazy {
+    "MonitoringData"
+}
+lateinit var internetSpeedCoeff: Double
+lateinit var lightsStatus: String
+lateinit var eventLog: String
+val medicalAssistanceAvailable: Boolean = true
+private var eventSafetyPlan: String = "Планы и процедуры для обеспечения безопасности мероприятия, включая планы эвакуации и протоколы чрезвычайных ситуаций"
+lateinit val eventRegistrationNumber: String
+private val maxNoiseLevel: Int = "80"
+var noiseLimitExeeded: Boolean = false
+private val pressProtocol: String = "План взаимодействия с прессой"
+lateinit var hackatonPublicReport: String
+var legalPermitsStatus: String = "Статус получения всех необходимых разрешений для проведения мероприятия"
+var specialEquipmentAvailable: Boolean = false
+var eventPartners: String = "Список партнеров мероприятия"
+lateinit var publicReport: String
+private val prizeDistributionRules: String = "Внутренние правила распределения призов"
+val prizeList: String = "Список разыгрываемых призов"
+private val emergencyContacts: String = "Контактная информация экстренных служб, медицинского персонала и других важных служб, для внутреннего пользования"
+val specialConditions: String = "Особые условия для участников с ограниченными возможностями"
+lateinit var participantsMood: String
+lateinit var detailedHackatonPlan: String
+lateinit var specialGuest: String
+val maximumParticipantsCapacity: Int = 1000
+val timeLimitForProject: Int = 48
